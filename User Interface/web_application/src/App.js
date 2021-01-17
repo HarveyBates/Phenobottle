a3c2f82ee1e104cc31e684c829c39bfc88b0dc60
Harvey Bates <phenobottle@gmail.com>
2021-01-17 23:25:31 +1100

    added plot placeholder

--- User Interface/web_application/src/App.js
+++ User Interface/web_application/src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
 import Navbar from './navbar.jsx';
+import React, {useState} from 'react';
+import Plot from 'react-plotly.js';
 
 function App() {
-	const sayHello = () => {
-		console.log("Hello");
-	};
 
+	const [count, setCount] = useState(0);
+
+	const increment = () => {
+		setCount(count + 1);
+	};
+	
 	return (
 		<div>
 			<Navbar/>
-			<h1> Hello </h1>
-			<button onClick={sayHello}>Hello</button>
+			<main>
+				<div className="graph-card">
+					<Plot
+						useResizeHandler
+						style={{width: '100%', height: '100%'}}
+						data={[
+							{
+							x: [1, 2, 3, 4, 5, 6],
+							y: [1, 3, 6, 12, 24, 48],
+							type: 'scatter', 
+							mode: 'lines+markers',
+							marker:{color: 'red'},
+							}
+						]}
+						layout={{
+							autosize: true,
+						}}
+					/>
+					<h1>{count}</h1>
+					<button onClick={increment}>Add One</button>
+				</div>	
+			</main>
 		</div>
 	);
 }
