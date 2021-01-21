c771aa49858980842395505d8e303c181a9b1075
Harvey Bates <phenobottle@gmail.com>
2021-01-21 12:06:08 +1100

    rm cmake files

--- User Interface/web_application/backend/CMakeFiles/CheckIncludeFiles/HAVE_INTTYPES_H.c
+++
@@ -1,8 +0,0 @@
-/* */
-#include </usr/local/include/libssh2.h>
-#include <stdio.h>
-#include <inttypes.h>
-
-
-int main(void){return 0;}
-
--- User Interface/web_application/backend/CMakeFiles/CheckIncludeFiles/HAVE_STDIO_H.c
+++
@@ -1,7 +0,0 @@
-/* */
-#include </usr/local/include/libssh2.h>
-#include <stdio.h>
-
-
-int main(void){return 0;}
-
