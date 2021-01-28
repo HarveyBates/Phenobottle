727ccb0189d7cf81b323dabfd06e5edcd07cef7f
Harvey Bates <phenobottle@gmail.com>
2021-01-28 21:24:36 +1100

    switching to .NET

---
+++ User Interface/web_application/client/netcore/PhenobottleUI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PhenobottleUI.Models;
+
+namespace PhenobottleUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home() {
+            return View();
+		}
+
+        public IActionResult Dashboard() {
+            return View();
+		}
+
+        public IActionResult Phenobottles() {
+            return View();
+		}
+
+        public IActionResult Sensors() {
+            return View();
+		}
+
+        public IActionResult Environment() {
+            return View();
+		}
+
+        public IActionResult Settings() {
+            return View();
+		}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
