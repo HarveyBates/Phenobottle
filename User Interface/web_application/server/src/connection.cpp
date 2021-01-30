c2fe542727c184edb4abbb7f2e4d6d39f1145fd7
Harvey Bates <phenobottle@gmail.com>
2021-01-30 13:41:54 +1100

    working xor decryption for long and short frames

--- User Interface/web_application/server/src/connection.cpp
+++ User Interface/web_application/server/src/connection.cpp
@@ -1,6 +1,4 @@
 #include "connection.h"
-#include <string>
-#include <cstring>
 
 /* Shared pointer to keep connection between client and server while
  * async functions are running */
@@ -31,113 +29,80 @@ void Connection::handle_write(const asio::error_code& /*error*/,
 }
 
 void xor_decrypt(char* inBuffer){
-	/* [Reminder] This is where I will decode the incoming requests depending 
-	 * on their package length
+	/* Decode frame from incoming message using xor decryption.
+	 * A mask (4 byte) sequence of char's are used to decrypt the incoming data.
+	 * The positon of the mask varies with the size of the incoming data, 
+	 * therefore two functions are provided (<125 & 126).
 	 */
+	
+	// Create output array and fill to delete any previous values
 	char outBuffer[max_length];
 	std::fill_n(outBuffer, max_length, 0);
-	char* buffer;
-	unsigned int packet_length = 0;
-	unsigned char mask[4];
-	unsigned int offset = 0;			
-	std::string output;
+	
+	uint16_t packet_length = 0; // Length of package data
+	unsigned char mask[4]; // Used for xor decryption
+	unsigned int offset = 0; // Offset position of data based on data length
+	
 	// Ensures the data received is a message
 	if((unsigned char)inBuffer[0] == 0x81){
 
-		while(offset + 6 < (sizeof(&inBuffer)/sizeof(char))){
+		/* Get the length of the incoming data */
+		packet_length = ((unsigned char) inBuffer[1]) & 0x7f;
 
-			/* Get the length of the incoming data */
-			packet_length = ((unsigned char) inBuffer[1]) & 0x7f;
-			std::cout << "Original packet length: " << packet_length << std::endl;
+		if(packet_length == 126){
+			/* For packet sizes of 126:
+			 * byte 0: The 0x81 is just an indicator that a message received
+			 * byte 1: will be 0xfe
+			 * byte 2, 3: the length of the payload as a uint16 number
+			 * byte 4, 5, 6, 7: the 4 byte xor key to decrypt the payload
+			 * the rest: payload
+			 */
+		
+			// Get length of larger packet size
+			unsigned int lenByte1 = inBuffer[2];
+			unsigned int lenByte2 = inBuffer[3];
+			packet_length = ((lenByte1 << 8) & 0xFF00) + (lenByte2 & 0x00FF);
 
-			if(packet_length == 126){
-				/*
-				 * byte 0: The 0x81 is just an indicator that a message received
-				 * byte 1: will be 0xfe
-				 * byte 2, 3: the length of the payload as a uint16 number
-				 * byte 4, 5, 6, 7: the 4 byte xor key to decrypt the payload
-				 * the rest: payload
-				 */
-
-				unsigned int lenByte1 = inBuffer[offset + 2];
-				unsigned int lenByte2 = inBuffer[offset + 3];
-
-				packet_length = ((lenByte1 << 8) & 0xFF00) + (lenByte2 & 0x00FF);
-
-				// TODO Getting the right length now, but need to fix the mask 
-				// for these longer messages
-				std::cout << "Oversize packet length: " << packet_length << std::endl;
-
-				mask[0] = inBuffer[offset + 4];
-				mask[1] = inBuffer[offset + 5];
-				mask[2] = inBuffer[offset + 6];
-				mask[3] = inBuffer[offset + 7];
-
-				for(unsigned int i = 0; i < packet_length; i++){
-					int arrPos = offset + 8 + i;
-					outBuffer[i] = inBuffer[arrPos] ^ mask[i % 4];
-					//inBuffer[arrPos] ^= mask[i % 4];
-					//std::cout << inBuffer[arrPos] << std::endl;
-				}
-				
-				std::cout << "Input buffer: " << outBuffer << std::endl;
-
-				//output = std::string(inBuffer).substr(8, std::string(inBuffer).length());
-				
-				//strcpy(outBuffer, output.c_str());
-
-				offset += 8 + packet_length;
-
-			}
-			else if(packet_length == 127){
-				// TODO add handle for package size: 127			
-				size_t payload_length = (((size_t)inBuffer[2] << 56) +
-						((size_t)inBuffer[3] << 48) +
-						((size_t)inBuffer[4] << 40) + 
-						((size_t)inBuffer[5] << 32) +
-						((size_t)inBuffer[6] << 24) + 
-						((size_t)inBuffer[7] << 16) +
-						((size_t)inBuffer[8] << 8) +
-						(size_t)inBuffer[9]);
-				std::cout << payload_length << std::endl;
-			}
-			else{
-				/*
-				 * byte 0: The 0x81 is just an indicator that a message received
-				 * byte 1: the 0x8a is the length, substract 0x80 from it, 0x0A == 10
-				 * byte 2, 3, 4, 5: the 4 byte xor key to decrypt the payload
-				 * the rest: payload
-				 */
-
-				mask[0] = inBuffer[offset + 2];
-				mask[1] = inBuffer[offset + 3];
-				mask[2] = inBuffer[offset + 4];
-				mask[3] = inBuffer[offset + 5];
-				std::cout << "Packet Length: " << packet_length << std::endl;
-				std::cout << "Mask: " << mask << std::endl;
-
-				/* Use mask(key) to decode message from client 
-				 * Offset of 6 as this is the bytes after the mask
-				 */
-				for(unsigned int i = 0; i < packet_length; i++){
-					unsigned int arrPos = offset + 6 + i;
-					outBuffer[i] = inBuffer[arrPos] ^ mask[i % 4];
-					//inBuffer[arrPos] ^= mask[i % 4];
-				}
-				
-				std::cout << "Message: " << outBuffer << std::endl;
-
-				/* Take decoded input buffer (inBuffer) and extract only the data 
-				* (not the mask)
-				 */
-				//output = std::string(inBuffer).substr(6, std::string(inBuffer).length());
-				//strcpy(outBuffer, output.c_str());
-				
-				offset += 6 + packet_length;
-				//std::cout << "Message from client: " << output << std::endl;
+			// Get mask for 126 pkt size data
+			mask[0] = inBuffer[4];
+			mask[1] = inBuffer[5];
+			mask[2] = inBuffer[6];
+			mask[3] = inBuffer[7];
+			
+			// Set a 8 byte offset to read mask first
+			offset = 8;
+	
+			// Interpret data after the offset and decrypt based on mask keys
+			for(unsigned int i = 0; i < packet_length; i++){
+				int arrPos = offset + i;
+				outBuffer[i] = inBuffer[arrPos] ^ mask[i % 4];
 			}
 		}
-		std::cout << outBuffer << std::endl;
+		else{
+			/* For packet sizes < 125
+			 * byte 0: The 0x81 is just an indicator that a message received
+			 * byte 1: the 0x8a is the length, substract 0x80 from it, 0x0A == 10
+			 * byte 2, 3, 4, 5: the 4 byte xor key to decrypt the payload
+			 * the rest: payload
+			 */
+			
+			mask[0] = inBuffer[2];
+			mask[1] = inBuffer[3];
+			mask[2] = inBuffer[4];
+			mask[3] = inBuffer[5];
+			
+			// Set a 6 bit offset (after mask)
+			offset = 6;
+
+			/* Use mask(key) to decode message from client 
+			 * Offset of 6 as this is the bytes after the mask
+			 */
+			for(unsigned int i = 0; i < packet_length; i++){
+				unsigned int arrPos = offset + i;
+				outBuffer[i] = inBuffer[arrPos] ^ mask[i % 4];
+			}
+		}
+		std::cout << "Output buffer: " << outBuffer << std::endl;
 	}
 }
 
@@ -146,28 +111,23 @@ void Connection::do_read(){
 	/* Shared ptr to keep connection */
 	auto self(shared_from_this());
 
-	/* Reset _data array with zeros
-	 * TODO There is proberbly a better way to clear the array
-	 */
+	// Reset _data array with zeros
 	std::fill_n(_data, max_length, 0);
 	
 	/* Read incoming data */
 	socket_.async_read_some(asio::buffer(_data, max_length), 
 			[this, self](std::error_code err, std::size_t length){
 		if(!err){
-			std::cout << "Recieved data: " << std::endl;
-
+			// Store data as a char for deryption
 			char* inBuffer = _data;
-			/* TODO Need to have two functions:
-			* 1. For incoming connection request
-			* 2. For incoming data
-			*/
+
+			// If handshake has already occured no need to decrypt this message
 			if(handshake){
 				xor_decrypt(inBuffer);
 			}
 			else{
-				std::cout << _data << std::endl;
-				handshake = true;
+				std::cout << "\nHandshake: \n" << _data << std::endl;
+				handshake = true; // Handshake has occured
 			}
 			/* Begin write funciton to keep connection */
 			do_write();
