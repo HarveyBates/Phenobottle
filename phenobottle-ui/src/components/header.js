1c811aede686b0073b5c85efaccd90153292a5c2
HarveyBates <phenobottle@gmail.com>
2021-06-27 11:33:45 +1000

    updating ui

--- phenobottle-ui/src/components/header.js
+++ phenobottle-ui/src/components/header.js
@@ -1,4 +1,8 @@
 import "./header.css";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(fas);
 
 function Header() {
 	return (
@@ -6,18 +10,22 @@ function Header() {
 			<nav className="navbar">
 				<ul className="navlist"> 
 					<li className="navitem">
+						<FontAwesomeIcon class="navimg" icon={["fas", "flask"]}/>
 						<a className="navlink" href="/"><span>Devices</span></a>
 					</li>
-			
+
 					<li className="navitem">
+						<FontAwesomeIcon class="navimg" icon={["fas", "sliders-h"]}/>
 						<a className="navlink" href="/"><span>Control</span></a>
 					</li>
 
 					<li className="navitem">
+						<FontAwesomeIcon class="navimg" icon={["fas", "clone"]}/>
 						<a className="navlink" href="/"><span>Experiment</span></a>
 					</li>
-					
+
 					<li className="navitem">
+						<FontAwesomeIcon class="navimg" icon={["fas", "cogs"]}/>
 						<a className="navlink" href="/"><span>Settings</span></a>
 					</li>
 				</ul>
