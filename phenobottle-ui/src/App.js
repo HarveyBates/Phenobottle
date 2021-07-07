d2e1c7debc9de517864cb42a353f9fdb6751438f
HarveyBates <phenobottle@gmail.com>
2021-07-07 21:50:08 +1000

    updating login page

--- phenobottle-ui/src/App.js
+++ phenobottle-ui/src/App.js
@@ -1,20 +1,22 @@
 import { UserContext } from "./containers/usercontext.js";
 import React, { useState, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import { Navbar } from './components/navbar/navbar.js';
 import { Login } from "./containers/pages/login.js";
 import { DeviceControl } from "./containers/pages/device-control.js";
 
 function App() {
+	const [username, setUsername] = useState("username");
+	const [password, setPassword] = useState("password");
 	return (
 		<Router>
-		<div className="App">
-			<UserContext.Provider value="Hello">
-				<Route path="/" exact component={ Login }/>
-				<Route path="/device-control" exact component={ DeviceControl }/>
-			</UserContext.Provider>
-		</div>
+			<div className="App">
+				<UserContext.Provider value={{username, setUsername, password, setPassword}}>
+					<Route path="/" exact component={ Login }/>
+					<Route path="/device-control" exact component={ DeviceControl }/>
+				</UserContext.Provider>
+			</div>
 		</Router>
 	);
 }
--- phenobottle-ui/src/containers/pages/login.js
+++ phenobottle-ui/src/containers/pages/login.js
@@ -1,17 +1,56 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { UserContext } from "../usercontext.js";
 import "../styles/login.css";
-import logo from "../../images/logo.png";
+import Algae from "../../images/algae.png";
 
 export function Login(){
-	const msg = useContext(UserContext);
+	const {username, setUsername} = useContext(UserContext);
+	const {password, setPassword} = useContext(UserContext);
+	const [siUsername, setsiUsername] = useState("");
+	const [siPassword, setsiPassword] = useState("");
+
+	const updateUsername = (event) => {
+		setsiUsername(event.target.value);
+	};
+
+	const updatePassword = (event) => {
+		setsiPassword(event.target.value);
+	}
+
 	return (
 		<div className="card">
-			<div className="connection">
-				<img alt="phenobottle logo"
-					src={logo}
-					className="logo"/>
+			<div className="signin">
+				<h1>Sign in</h1>
 				<form className="loginform">
+					<label className="loginlabel">
+						<input type="text" 
+							placeholder="Username" required value={siUsername} onChange={updateUsername}/>
+					</label>
+					<label className="loginlabel">
+						<input type="password" 
+							placeholder="Password" required value={siPassword} onChange={updatePassword}/>
+					</label>
+					<div className="loginbuttons">
+						<button onClick={ async () => { 
+							setUsername({siUsername})
+							setPassword({siPassword})
+							}
+						} 
+						className="submitbutton">Login</button>
+						<button className="submitbutton">Sign up</button>
+					</div>
+				</form>
+			</div>
+			<div className="signup">
+				<img alt="algae"
+					src={Algae}
+					className="algae"/>
+				<h1>Sign up</h1>
+				<form className="loginform">
+					<label className="loginlabel">
+						<input type="text" name="name" 
+							placeholder="Name"/>
+					</label>
 					<label className="loginlabel">
 						<input type="text" name="username" 
 							placeholder="Username"/>
@@ -19,6 +58,10 @@ export function Login(){
 					<label className="loginlabel">
 						<input type="password" name="password" 
 							placeholder="Password"/>
+					</label>
+					<label className="loginlabel">
+						<input type="password" name="retypepassword" 
+							placeholder="Retype Password"/>
 					</label>
 					<div className="loginbuttons">
 						<button className="submitbutton">Login</button>
